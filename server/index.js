08aa1dcb45982b593c8bf8053f08376596917c25
Shaun <shaun.tung@gmail.com>
2020-02-13 15:42:44 -0500

    added backend boilerplate

--- server/index.js
+++ server/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const db = require('./db');
 const port = process.env.PORT || 3000;
+module.exports = app;
 
 const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+// Static Middleware
 app.use(express.static(DIST_DIR));
 
+// Logging Middleware
+app.use(morgan('dev'));
+
+// Parsing Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// auth and api routes
+app.use('/auth', require('./auth'));
+app.use('/api', require('./api'));
+
 app.get('/', (req, res) => {
-  res.sendFile(HTML_FILE);
+    res.sendFile(HTML_FILE);
 });
 app.listen(port, function() {
-  console.log('Finance App listening on port: ' + port);
+    console.log('Finance App listening on port: ' + port);
 });
